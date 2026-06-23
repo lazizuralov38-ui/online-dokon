bfd0b3725f7b1de238211c04f92899f87e498c42
lazizuralov38-ui <lazizuralov38@gmail.com>
2026-06-23 21:02:26 +0300

    Add files via upload

--- oo/settings.py
+++ oo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,17 +74,13 @@ WSGI_APPLICATION = 'oo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',           
-    }
-}
 
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://online_dokon_db_user:oHi0R2uKF9Bzx8CRrWahFltL...[qolgan qismi]'
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
