155b0aa2dd30de66388ec1c71e2d2efa68bae279
lazizuralov38-ui <lazizuralov38@gmail.com>
2026-06-24 14:50:08 +0300

    Update settings.py

--- oo/settings.py
+++ oo/settings.py
@@ -84,5 +84,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
